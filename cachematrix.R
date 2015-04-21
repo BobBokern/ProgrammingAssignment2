7329674ec9ffa3e9523dec6b308b512f851562f7
BobBokern <bob.bokern@gmail.com>
2015-04-21 10:26:40 +0200

    first attempt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below will compute and cache the inverse of a matrix
+## (assuming that the matrix supplied is always invertible)
 
-## Write a short comment describing this function
+## The function makeCacheMatrix creates a special matrix object,
+## that is able to cache its own inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(Inverse) inverse <<- Inverse
+  getInverse <- function() inverse
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve computes and returns the inverse of a special matrix returned by the 
+## function makeCacheMatrix (see above). When the matrix is still the same and the inverse of this matrix has 
+## already been calculated, the function should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverseOfX <- x$getInverse()
+  if(!is.null(inverseOfX)) {
+    message("Getting inverse from the cache...")
+    return(inverseOfX)
+  }
+  data <- x$get()
+  inverseOfX <- solve(data, ...)
+  x$setInverse(inverseOfX)
+  inverseOfX  
 }
+
+
+
+## Sample run:
+x = rbind(c(1, -1/4), c(-1/4, 1))
+m = makeCacheMatrix(x)
+m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## No cache in the first run
+cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving from the cache in the second run
+cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
